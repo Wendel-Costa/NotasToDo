3c902c3c728b60e6008fb753e5c9d7f9784c94aa
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-09-08 19:19:17 -0300

    Fazendo crud básico

--- backend/src/controllers/notaController.js
+++ backend/src/controllers/notaController.js
@@ -10,11 +10,41 @@ class NotaController {
         }
     }
 
+    static async listarNotaPorId (req,res) {
+        const idNota = req.params.id
+        try{
+            const listaNotas = await nota.findById(idNota);
+            res.status(200).json(listaNotas);
+        } catch(erro){
+            res.status(500).json(erro);
+        }
+    }
+
     static async criarNota (req,res) {
         try{
             const notaCriada = new nota(req.body)
             const novaNota = await notaCriada.save();
             res.status(200).json(novaNota);
+        } catch(erro){
+            res.status(500).json(erro);
+        }
+    }
+
+    static async atualizarNota (req,res) {
+        const idNota = req.params.id
+        try{
+            await nota.findByIdAndUpdate(idNota, req.body);
+            res.status(200).json({message: "Nota atualizada!"});
+        } catch(erro){
+            res.status(500).json(erro);
+        }
+    }
+
+    static async excluirNota (req,res) {
+        const idNota = req.params.id
+        try{
+            await nota.findByIdAndDelete(idNota);
+            res.status(200).json({message: "Nota apagada!"});
         } catch(erro){
             res.status(500).json(erro);
         }
--- backend/src/routes/notasRoutes.js
+++ backend/src/routes/notasRoutes.js
@@ -4,6 +4,9 @@ import NotaController from "../controllers/notaController.js";
 const routes = express.Router();
 
 routes.get("/notas", NotaController.listarNotas);
+routes.get("/notas/:id", NotaController.listarNotaPorId);
 routes.post("/notas", NotaController.criarNota);
+routes.put("/notas/:id", NotaController.atualizarNota);
+routes.delete("/notas/:id", NotaController.excluirNota);
 
 export default routes;
